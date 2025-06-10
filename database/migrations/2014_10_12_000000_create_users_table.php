e946f848f466a583d7e682a9f8631b4433cefb32
amirpeyravan <141130613+AmirPeyravan@users.noreply.github.com>
2025-06-10 12:04:46 +0330

    set content

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-
+            Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->char('mobile', 11)->nullable();
+            $table->enum('role', ['admin', 'user']);
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
     }
 
     /**
